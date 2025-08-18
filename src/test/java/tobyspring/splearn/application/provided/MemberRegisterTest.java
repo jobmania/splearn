e0b4b923eee28555f29c0a5bd74fb182ccfb663d
wcson <itnj.wcson@gmail.com>
2025-08-18 19:12:37 +0900

    Stub 및 Mock 역할 Stub - 내부 구현 x, 내부 값 검증 x Mock - 내부 구현 후 내부 값 검증 가능 --------- Mokito를 통해 테스트 검증 가능

---
+++ src/test/java/tobyspring/splearn/application/provided/MemberRegisterTest.java
@@ -0,0 +1,90 @@
+package tobyspring.splearn.application.provided;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.test.util.ReflectionTestUtils;
+import tobyspring.splearn.application.MemberService;
+import tobyspring.splearn.application.required.EmailSender;
+import tobyspring.splearn.application.required.MemberRepository;
+import tobyspring.splearn.domain.Email;
+import tobyspring.splearn.domain.Member;
+import tobyspring.splearn.domain.MemberFixture;
+import tobyspring.splearn.domain.MemberStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+
+class MemberRegisterTest {
+
+    @Test
+    void registerTestStub() {
+        MemberRegister register = new MemberService(
+                new MemberRepositoryStub(),
+                new EmailSenderStub(),
+                MemberFixture.createPasswordEncoder()
+        );
+
+        Member memberRegister = register.register(MemberFixture.createMemberRegisterRequest());
+
+        assertThat(memberRegister.getId()).isNotNull();
+        assertThat(memberRegister.getStatus()).isEqualTo(MemberStatus.PENDING);
+
+    }
+
+
+    @Test
+    void registerTestMock() {
+
+
+        EmailSender emailSenderMock = Mockito.mock(EmailSender.class);
+
+        MemberRegister register = new MemberService(
+                new MemberRepositoryStub(),
+                emailSenderMock,
+                MemberFixture.createPasswordEncoder()
+        );
+
+        Member memberRegister = register.register(MemberFixture.createMemberRegisterRequest());
+
+        assertThat(memberRegister.getId()).isNotNull();
+        assertThat(memberRegister.getStatus()).isEqualTo(MemberStatus.PENDING);
+
+//        assertThat(emailSenderMock.tos.size()).isEqualTo(1L);
+//        assertThat(emailSenderMock.tos.getFirst()).isEqualTo(memberRegister.getEmail());
+
+        Mockito.verify(emailSenderMock).send(eq(memberRegister.getEmail()) ,any(),any());
+    }
+
+
+    static class MemberRepositoryStub implements MemberRepository {
+        @Override
+        public Member save(Member member) {
+
+            ReflectionTestUtils.setField(member,"id",1L);
+            return member;
+        }
+
+    }
+
+    static class EmailSenderStub implements tobyspring.splearn.application.required.EmailSender{
+
+        @Override
+        public void send(Email email, String subject, String body) {
+        }
+    }
+
+    static class EmailSenderMock implements tobyspring.splearn.application.required.EmailSender{
+
+        List<Email> tos = new ArrayList<>();
+
+        @Override
+        public void send(Email email, String subject, String body) {
+            tos.add(email);
+        }
+    }
+
+}
--- src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
+++ src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
@@ -5,9 +5,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import tobyspring.splearn.domain.Member;
 import tobyspring.splearn.domain.MemberFixture;
-import tobyspring.splearn.domain.MemberRegisterRequest;
 
 import static tobyspring.splearn.domain.MemberFixture.createPasswordEncoder;
 
@@ -29,7 +29,18 @@ class MemberRepositoryTest {
         memberRepository.save(member);
         Assertions.assertThat(member.getId()).isNotNull();
         entityManager.flush();
+    }
 
+    @Test
+    void duplicateEmailFail(){
+        Member member = Member.register(MemberFixture.createMemberRegisterRequest(), createPasswordEncoder());
+
+        memberRepository.save(member);
+
+        Member member2 = Member.register(MemberFixture.createMemberRegisterRequest(), createPasswordEncoder());
+
+        Assertions.assertThatThrownBy(() -> memberRepository.save(member2))
+                .isInstanceOf(DataIntegrityViolationException.class);
     }
 
 }
